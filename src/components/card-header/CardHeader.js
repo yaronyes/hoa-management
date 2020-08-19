0c069bd88df43d6301d3094c0e3fdc00584a8adb
yaronyes <yaronyes@hotmail.com>
2020-08-19 15:22:10 +0300

    adding icon to card header

--- src/components/card-header/CardHeader.js
+++ src/components/card-header/CardHeader.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './CardHeader.css';
+import { MDBIcon } from 'mdbreact';
 
-const CardHeader = ({ id, toggleCollapse, headerText }) => {
+
+const CardHeader = ({ id, toggleCollapse, color, headerText, icon }) => {
+    const colorId = color ? color : 'blue lighten-3';
 
     return (
         <div 
         onClick={() => toggleCollapse(id)}
-        className='card-header text-uppercase blue lighten-3 z-depth-1'
+        className={'card-header text-uppercase z-depth-1 ' + colorId}
         role="tab"
         >
             <span className='white-text font-weight-bold'>
                 {headerText}
             </span>
+            <MDBIcon icon={icon}/>
         </div>
     );
 }
