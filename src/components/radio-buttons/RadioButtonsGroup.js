1d683bba227b957e993df2f8c3960c86447a94b5
yaronyes <yaronyes@hotmail.com>
2020-08-20 09:52:04 +0300

    adding RadioButtonsGroup component

---
+++ src/components/radio-buttons/RadioButtonsGroup.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import './RadioButtonsGroup.css';
+
+export default function RadioButtonsGroup({ label, radioBtnInfo, defaultSelect, onChange, isHorizontal=true }) {
+  const [value, setValue] = React.useState(defaultSelect);
+
+  const handleChange = (event) => {
+    onChange(event.target.value);
+
+    setValue(event.target.value);
+  };
+
+  const displayBtns = radioBtnInfo.map(btn => <FormControlLabel value={btn.value} control={<Radio />} label={btn.label} />)
+
+  return (
+    <div className="radio-group-btn">
+      <FormLabel htmlFor="radio-group" className="group-label">{label}</FormLabel>
+      <RadioGroup aria-label={label} id="radio-group" value={value} onChange={handleChange} row={isHorizontal}>
+        {displayBtns}
+      </RadioGroup>
+    </div>
+  );
+}
