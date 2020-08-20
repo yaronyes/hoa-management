e4da28b0bcb002e962b6d97f07df84708e470395
yaronyes <yaronyes@hotmail.com>
2020-08-20 08:01:26 +0300

    added label to FilterBox

--- src/components/filter/FilterBox.js
+++ src/components/filter/FilterBox.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { MDBInput } from 'mdbreact';
 import './FilterBox.css';
 
-const FilterBox = ({ onFilterChanged }) => {
+const FilterBox = ({ onFilterChanged, label }) => {
     const [filter, setFilter] = useState("");
 
     const filterChanged = event => {
@@ -13,7 +13,7 @@ const FilterBox = ({ onFilterChanged }) => {
 
     return (
         <div className="filter-box">
-            <MDBInput label="Filter" icon="search" value={filter} onChange={filterChanged}/>
+            <MDBInput label={label ? label : "Filter"} icon="search" value={filter} onChange={filterChanged}/>
         </div>
         
     );
