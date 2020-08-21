b8775fbc1ba7cb57c16b3b3d1706becf9d0ec40a
yaronyes <yaronyes@hotmail.com>
2020-08-21 09:23:59 +0300

    added options component

---
+++ src/components/option/Options.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { MDBInput, MDBIcon } from 'mdbreact';
+import './Options.css';
+
+const Options = () => {
+    const [options, setOptions] = useState([
+        {
+            name: "option1",
+            value: ""
+        },
+        {
+            name: "option2",
+            value: ""
+        }
+    ]);
+    const [iconColor, setIconColor] = useState("");
+
+    const optionChanged = (event) => {
+        setOptions(options.map(option => {
+            if(option.name === event.target.name) {
+                return {
+                    ...option,
+                    value: event.target.value
+                }
+            } else {
+                return option;
+            }
+        }));
+    };
+
+    const addOption = () => {
+        setOptions([
+            ...options,
+            {
+                name: `option${options.length}`,
+                value: ""
+            }
+        ]);
+    };
+
+    const displayOptions = options.map((option, i) => <MDBInput key={i} type="text" icon="pencil-alt" value={option.value} onChange={optionChanged} name={option.name} />);
+
+    return (
+        <div className="options">
+            {displayOptions}
+        <div className="add-option">
+            <MDBIcon icon="plus" size="2x" onClick={addOption} className={"add-option-icon " + iconColor} onMouseOver={() => setIconColor("blue-text")} onMouseLeave={() => setIconColor("")} /> Add Option            
+        </div>
+        </div>
+    );
+};
+
+export default Options;
