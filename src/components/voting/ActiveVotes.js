53bac99f93bc6772cbefa1b76e824e6d37e67afb
yaronyes <yaronyes@hotmail.com>
2020-08-22 15:19:53 +0300

    adding active votes component

---
+++ src/components/voting/ActiveVotes.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import AddUpdateVoting from '../../components/voting/AddUpdateVoting';
+import VotingCard from '../../components/voting/VotingCard';
+import RoundedBtn from '../../components/rounded-button/RoundedBtn';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// import { getVoting } from '../../actions/votingActions';
+
+const ActiveVotes = ({ activeVotes }) => {
+    const [modal, setModel] = useState(false);
+    const [collapseID, setCollapseID] = useState(0);    
+    const [selectedVoting, setSelectedVoting] = useState();
+
+    // useEffect(() => {
+    //     if(votes.length === 0) {
+    //         getVoting();
+    //     }      
+    //   }, [votes]);
+    
+    const toggle = () => {
+        setModel(!modal);
+    }
+
+    const openAddUpdateModal = message => {
+        setSelectedVoting(message);      
+        toggle();
+      }
+
+    const toggleCollapse = newCollapseID => setCollapseID(collapseID !== newCollapseID ? newCollapseID : '');
+    
+    // const activeVotes = votes.filter(voting => voting.isActiveVoting());
+    const displayActiveVotes = activeVotes.map(item => <VotingCard key={item._id} toggleCollapse={toggleCollapse} voting={item} openID={collapseID} onUpdateMessage={openAddUpdateModal} activeVoting={item.isActiveVoting()}/>);
+
+    // const doneVotes = votes.filter(voting => !voting.isActiveVoting());
+    // const displayDoneVotes = doneVotes.map(item => <VotingCard key={item._id} toggleCollapse={toggleCollapse} voting={item} openID={collapseID} onUpdateMessage={openAddUpdateModal} activeVoting={item.isActiveVoting()}/>);
+      
+    return (
+        <div className="active-votes">
+            {/* <MDBCol> */}
+                <MDBRow>
+                    <MDBCol>
+                        <RoundedBtn color="primary" onClick={() => toggle()} icon="user-plus" caption="Create New Voting"/>
+                    </MDBCol>                            
+                </MDBRow>
+                <MDBRow>                            
+                    <MDBCol>
+                        Active Votes
+                        {displayActiveVotes}
+                    </MDBCol>                            
+                </MDBRow>
+                <MDBRow>
+                    <MDBCol>
+                        Voting
+                    </MDBCol>                            
+                </MDBRow>
+            {/* </MDBCol>        */}
+            <AddUpdateVoting modal={modal} toggle={toggle} />   
+        </div>
+    );
+}
+
+ActiveVotes.propTypes = {
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired,
+    votes: PropTypes.array.isRequired,
+    //getVoting: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors,
+    votes: state.voting
+});
+
+export default connect(mapStateToProps, { })(ActiveVotes);
--- src/pages/voting/VotingPage.js
+++ src/pages/voting/VotingPage.js
@@ -6,6 +6,7 @@ import RoundedBtn from '../../components/rounded-button/RoundedBtn';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getVoting } from '../../actions/votingActions';
+import ActiveVotes from '../../components/voting/ActiveVotes';
 
 const VotingPage = ({ getVoting, votes }) => {
     const [modal, setModel] = useState(false);
@@ -30,7 +31,7 @@ const VotingPage = ({ getVoting, votes }) => {
     const toggleCollapse = newCollapseID => setCollapseID(collapseID !== newCollapseID ? newCollapseID : '');
     
     const activeVotes = votes.filter(voting => voting.isActiveVoting());
-    const displayActiveVotes = activeVotes.map(item => <VotingCard key={item._id} toggleCollapse={toggleCollapse} voting={item} openID={collapseID} onUpdateMessage={openAddUpdateModal} activeVoting={item.isActiveVoting()}/>);
+    //const displayActiveVotes = activeVotes.map(item => <VotingCard key={item._id} toggleCollapse={toggleCollapse} voting={item} openID={collapseID} onUpdateMessage={openAddUpdateModal} activeVoting={item.isActiveVoting()}/>);
 
     const doneVotes = votes.filter(voting => !voting.isActiveVoting());
     const displayDoneVotes = doneVotes.map(item => <VotingCard key={item._id} toggleCollapse={toggleCollapse} voting={item} openID={collapseID} onUpdateMessage={openAddUpdateModal} activeVoting={item.isActiveVoting()}/>);
@@ -40,7 +41,7 @@ const VotingPage = ({ getVoting, votes }) => {
             <MDBContainer>
                 <MDBRow>
                     <MDBCol lg="6">
-                        <MDBRow>
+                        {/* <MDBRow>
                             <MDBCol>
                                 <RoundedBtn color="primary" onClick={() => toggle()} icon="user-plus" caption="Create New Voting"/>
                             </MDBCol>                            
@@ -55,7 +56,8 @@ const VotingPage = ({ getVoting, votes }) => {
                             <MDBCol>
                                 Voting
                             </MDBCol>                            
-                        </MDBRow>
+                        </MDBRow> */}
+                        <ActiveVotes activeVotes={activeVotes}/>
                     </MDBCol>
                     <MDBCol lg="6">
                         <MDBRow>
