034d95df2da919b421c8e8eafa2dfb896b9abec1
yaronyes <yaronyes@hotmail.com>
2020-08-22 13:11:45 +0300

    added new pie chart component

---
+++ src/components/charts/ToolTipPieChart.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { MDBContainer } from "mdbreact";
+import { PieChart } from "react-minimal-pie-chart";
+import ReactTooltip from "react-tooltip";
+
+const defaultLabelStyle = {
+  fontSize: "10px",
+  fontFamily: "sans-serif",
+};
+
+const ToolTipPieChart = ({ data, isPercentage=false, header }) => {
+  const [hovered, setHovered] = useState(null);
+
+  const makeTooltipContent = (entry) => {
+    return `${entry.title}`;
+  };
+
+  return (
+    <div data-tip="" data-for="chart">
+      <h6>{header}</h6>
+      <PieChart
+        data={data}
+        // style={{ height: "100px" }}        
+        label={({ dataEntry }) => isPercentage ? Math.round(dataEntry.percentage) + '%' : dataEntry.value}
+        labelStyle={defaultLabelStyle}
+        onMouseOver={(_, index) => {
+          setHovered(index);
+        }}
+        onMouseOut={() => {
+          setHovered(null);
+        }}
+      />
+      <ReactTooltip
+        id="chart"
+        getContent={() =>
+          typeof hovered === "number" ? makeTooltipContent(data[hovered]) : null
+        }
+      />
+    </div>
+  );
+};
+
+export default ToolTipPieChart;
