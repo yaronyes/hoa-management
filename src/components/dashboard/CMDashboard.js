d3f205a6d17009b891480ac0aac5fa8a2cfa5e1d
yaronyes <yaronyes@hotmail.com>
2020-08-23 18:43:39 +0300

    setting the  comunnity manager dashboard layout

--- src/components/dashboard/CMDashboard.js
+++ src/components/dashboard/CMDashboard.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import React, { useState } from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
-const CMDashboard = () => { 
+const CMDashboard = () => {
   const [collapseID, setCollapseID] = useState(0);
 
-  const toggleCollapse = newCollapseID => setCollapseID(collapseID !== newCollapseID ? newCollapseID : '');
-
+  const toggleCollapse = (newCollapseID) => setCollapseID(collapseID !== newCollapseID ? newCollapseID : "");
+    
   return (
-      <div>
-          <MDBContainer>
-              <MDBRow>
-                <MDBCol>
-                  New Reported Issues
-                </MDBCol>                  
-                <MDBCol>
-                  Overdue Issues
-                </MDBCol>
-              </MDBRow>   
-              <MDBRow>
-                <MDBCol>
-                  Active Voting Percentage
-                </MDBCol>                  
-              </MDBRow>     
-          </MDBContainer>
-      </div>
+    <div>
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol>
+            <MDBRow>
+              <MDBCol>New Reported Issues</MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol></MDBCol>
+            </MDBRow>
+          </MDBCol>
+          <MDBCol>
+            <MDBRow>
+              <MDBCol>Overdue Issues</MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol></MDBCol>
+            </MDBRow>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol>
+            <MDBRow>
+              <MDBCol>Active Voting Percentage</MDBCol>
+              <MDBCol></MDBCol>
+            </MDBRow>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
-}
-
+};
 
 export default CMDashboard;
