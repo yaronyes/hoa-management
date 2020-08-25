833d18fcc7fec58ff32e20c53153bdc076247cc5
yaronyes <yaronyes@hotmail.com>
2020-08-25 22:12:36 +0300

    local homepage image

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -5,15 +5,8 @@ const HomePage = () => {
 
     return (
         <div className="home-page">
-            {/* <MDBView>
-                <img
-                src="https://mdbootstrap.com/img/Others/documentation/img%20(131)-mini.jpg"
-                className="img-fluid"
-                alt=""
-                />
-            </MDBView> */}
-            {/* <MDBView src="https://mdbootstrap.com/img/Photos/Others/img%20(50).jpg"> */}
-            <MDBView src="https://www.prattattorneys.com/wp-content/uploads/2019/01/HOA-photo-2.jpg">
+            {/* https://www.prattattorneys.com/wp-content/uploads/2019/01/HOA-photo-2.jpg */}
+            <MDBView src="HOA-photo-2.jpg">
                 <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
                 <h2>This Navbar is fixed</h2>
                 <h5>It will always stay visible on the top, even when you scroll down</h5>
