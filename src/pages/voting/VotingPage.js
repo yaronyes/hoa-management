cad54f9d59e6dd2268dbb665194d078ba326928a
yaronyes <yaronyes@hotmail.com>
2020-08-25 17:01:46 +0300

    cleanup

--- src/pages/voting/VotingPage.js
+++ src/pages/voting/VotingPage.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
-import AddUpdateVoting from '../../components/voting/AddUpdateVoting';
 import VotingCard from '../../components/voting/VotingCard';
-import RoundedBtn from '../../components/rounded-button/RoundedBtn';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getVoting } from '../../actions/votingActions';
 import ActiveVotes from '../../components/voting/ActiveVotes';
-import FilterBox from '../../components/filter/FilterBox';
 import './VotingPage.css';
 import selectVoting from '../../selectors/votingSelector';
 import VotingFilter from '../../components/voting/VotingFilter';
 
 const VotingPage = ({ getVoting, votes, filteredVoting }) => {
-    //const [modal, setModel] = useState(false);
     const [collapseID, setCollapseID] = useState(0);
-    const [filterText, setFilter] = useState("");   
-    //const [selectedVoting, setSelectedVoting] = useState();
 
     useEffect(() => {
         if(votes.length === 0) {
@@ -24,44 +18,15 @@ const VotingPage = ({ getVoting, votes, filteredVoting }) => {
         }      
       }, [votes]);
     
-    // const toggle = () => {
-    //     setModel(!modal);
-    // }
-
-    // const openAddUpdateModal = message => {
-    //     setSelectedVoting(message);      
-    //     toggle();
-    //   }
-
     const toggleCollapse = newCollapseID => setCollapseID(collapseID !== newCollapseID ? newCollapseID : '');
     
-    //const activeVotes = votes.filter(voting => voting.isActiveVoting());    
-
-    //const doneVotes = votes.filter(voting => !voting.isActiveVoting());
-    //const filter = doneVotes.filter(item => (item.title.toLowerCase().includes(filterText.toLowerCase().trim())) || (item.details.toLowerCase().includes(filterText.toLowerCase().trim())));
     const displayDoneVotes = filteredVoting.map(item => <VotingCard key={item._id} toggleCollapse={toggleCollapse} voting={item} openID={collapseID} /*onUpdateMessage={openAddUpdateModal}*/ /*isActiveVoting={item.isActiveVoting()}*//>);
       
     return (
         <div className="voting-page">
             <MDBContainer>
                 <MDBRow>
-                    <MDBCol lg="6">
-                        {/* <MDBRow>
-                            <MDBCol>
-                                <RoundedBtn color="primary" onClick={() => toggle()} icon="user-plus" caption="Create New Voting"/>
-                            </MDBCol>                            
-                        </MDBRow>
-                        <MDBRow>                            
-                            <MDBCol>
-                                Active Votes
-                                {displayActiveVotes}
-                            </MDBCol>                            
-                        </MDBRow>
-                        <MDBRow>
-                            <MDBCol>
-                                Voting
-                            </MDBCol>                            
-                        </MDBRow> */}
+                    <MDBCol lg="6">                        
                         <ActiveVotes />
                     </MDBCol>
                     <MDBCol lg="6">
@@ -70,10 +35,7 @@ const VotingPage = ({ getVoting, votes, filteredVoting }) => {
                                 <h1>Voting Results</h1>
                             </MDBCol>                            
                         </MDBRow>
-                        <MDBRow>
-                            {/* <MDBCol>
-                                <FilterBox onFilterChanged={(text) => setFilter(text)} label="Filter by text in Title and Details"/>
-                            </MDBCol>                             */}
+                        <MDBRow>                                                
                             <VotingFilter />
                         </MDBRow>                        
                         <MDBRow>
@@ -84,7 +46,6 @@ const VotingPage = ({ getVoting, votes, filteredVoting }) => {
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>        
-            {/* <AddUpdateVoting modal={modal} toggle={toggle} />    */}
         </div>
     );
 }
