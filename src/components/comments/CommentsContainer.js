76e1d2e4867c5deb8620e5b073f8102d61a71c88
yaronyes <yaronyes@hotmail.com>
2020-08-26 23:26:21 +0300

    adding scroll to comments

--- src/components/comments/CommentsContainer.js
+++ src/components/comments/CommentsContainer.js
@@ -1,28 +1,36 @@
 import React, { useState, useEffect, useRef } from 'react';
 import CommentPanel from './CommentPanel';
-import { Scrollbars } from 'react-custom-scrollbars';
+//import { Scrollbars } from 'react-custom-scrollbars';
 import './CommentsContainer.css'
+//import ScrollableFeed from 'react-scrollable-feed'
+import { css } from 'glamor';
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 const CommentsContainer = ({ comments }) => {
 
-    const commentsEndRef = useRef(null);
+    // const commentsEndRef = useRef(null);
     
-    useEffect(() => {
-        commentsEndRef.current.scrollIntoView({ behavior: "smooth" });
-      }, [comments]);
+    // useEffect(() => {
+    //     commentsEndRef.current.scrollIntoView({ behavior: "smooth" });
+    //   }, [comments]);
 
     const displayComments = comments.map(comment => <CommentPanel key={comment._id} text={comment.text}/>)
 
-    return (
-        // <div className="commentsWrapper" style={{ width: '100%' }}>
-        //     {displayComments}
-        //     <div ref={commentsEndRef} />
-        // </div>
-        <div style={{ width: '100%', height: comments.length === 0 ? 0 : 150 }}>
-            <Scrollbars style={{ width: '100%', height: '100%' }}>
-                {displayComments} 
-                <div ref={commentsEndRef} />               
-            </Scrollbars>
+    const ROOT_CSS = css({
+        height: '100%',
+        width: '100%'
+      });
+    
+    const style = { 
+        width: '100%', 
+        height: comments.length === 0 ? 0 : 100 
+    }
+
+    return (       
+        <div style={style}>
+            <ScrollToBottom className={ ROOT_CSS }>
+                {displayComments}                 
+            </ScrollToBottom>
         </div>
     );
 };
