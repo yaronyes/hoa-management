04b7c85feb7de906c91cf0551d5626fec0f67d9f
yaronyes <yaronyes@hotmail.com>
2020-08-27 22:02:36 +0300

    adding validation to add\update issue

--- src/components/issues/AddUpdateIssue.js
+++ src/components/issues/AddUpdateIssue.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { MDBContainer, MDBIcon, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBRow, MDBInput, MDBCol  } from "mdbreact";
+import React, { useState, useEffect, useRef } from "react";
+import { MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBRow, MDBInput, MDBCol  } from "mdbreact";
 import './AddUpdateIssue.css';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -8,34 +8,58 @@ import IssueModel from '../../models/IssueModel';
 import RoundedBtn from '../rounded-button/RoundedBtn';
 import DropDownSelect from '../select/DropDownSelect';
 import LoadImage from '../load-image/LoadImage';
-//import { Grid, TextField, OutlinedInput } from '@material-ui/core';
-//import AccountCircle from '@material-ui/icons/AccountCircle';
+import ValidationError from "../validation-errors/VelidetionError";
 
 const AddUpdateIssue = ({ modal, issueToUpdate, toggle, createIssue, updateIssue }) => {    
     const [title, setTitle] = useState("");
     const [details, setDetails] = useState("");
     const [priority, setPriority] = useState("");
     const [image, setImage] = useState();
-
-    // useEffect(() => {
-    //     toggle();
-    // }, [tenant]);
-    //console.log(issueToUpdate)
-        
+    const [validationErrors, setValidationErrors] = useState({});    
+    const formRef = useRef(null);
+    
     useEffect(() => {
+        if(modal) {
+            setValidationErrors({
+                title: false,
+                details: false,
+                priority: false,
+            });
+            
         setTitle(issueToUpdate ? issueToUpdate.title : "");
         setDetails(issueToUpdate ? issueToUpdate.details : "");
         setPriority(issueToUpdate ? issueToUpdate.priority : "");
-    }, [issueToUpdate]);
+        }        
+    }, [modal]);
 
-   const addUpdate = () => {
-    if(issueToUpdate) {
-        updIssue();
-    } else {
-        addIssue();
+    const addUpdate = () => {                       
+        if(!formRef.current.className.includes("was-validated")) { 
+            formRef.current.className += " was-validated";
+        }
+        
+        const errors = validateInput();
+        const numberOfErrors = Object.keys(errors).filter(key => validationErrors[key] === false);
+              
+        if(numberOfErrors.length === 0) {
+            if(issueToUpdate) {
+                updIssue();
+            } else {
+                addIssue();
+            }
+
+            toggle();
+        } else {
+            setValidationErrors(errors);  
+        }        
+    };
+
+    const validateInput = () => {
+        return {
+            title: title === "",
+            details: details === "",
+            priority: priority === ""
+        }
     }
-    toggle();
-  };
 
     const addIssue = () => {
         try{         
@@ -77,73 +101,82 @@ const AddUpdateIssue = ({ modal, issueToUpdate, toggle, createIssue, updateIssue
             
 
   return (
-      <div className="add-upd-tenant">
+      <div className="add-upd-issue">
         <MDBContainer>      
             <MDBModal isOpen={modal} toggle={toggle}>
                 <MDBModalHeader toggle={toggle}>{issueToUpdate ? "Update Issue" : "Create Issue"}</MDBModalHeader>
                 <MDBModalBody>
                 <MDBRow>
-                    <MDBCol md="9">
-                        <form>
-                        <div className="grey-text">
-                            <MDBInput
-                            label="Title"
-                            icon="text-height"
-                            group
-                            type="text"
-                            validate
-                            error="wrong"
-                            success="right"                  
-                            value={title}
-                            onChange={e => setTitle(e.target.value)}
-                            />
-                            <MDBInput
-                            type="textarea"
-                            label="Details"
-                            rows="4"
-                            icon="pencil-alt"
-                            // group
-                            value={details}
-                            onChange={e => setDetails(e.target.value)}
-                            />                            
-                            <DropDownSelect onChange={(priority) => setPriority(priority)} icon="exclamation" label="priority" 
-                            dropDownItems={[
-                                {
-                                    value: "urgent",
-                                    name: "urgent"
-                                },
-                                {
-                                    value: "important",
-                                    name: "important"
-                                },
-                                {
-                                    value: "normal",
-                                    name: "normal"
-                                }
-                            ]}
-                            defaultValue={priority}/>  
-                            <LoadImage fileCallback={fileCallback}/>                                                                                 
-                            {/* <OutlinedInput type="file"/> */}
-                            {/* <div >
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>         
-            <MDBIcon icon="file-upload" className="drop-down-select-ico"/>
-          </Grid>
-          <Grid item>
-            <TextField id="input-with-icon-grid" label="Load Image" type="file"/>
-          </Grid>
-        </Grid>
-      </div> */}
-                        </div>                
+                    <MDBCol md="10">
+                        <form ref={formRef}
+                            className="needs-validation"                       
+                            > 
+                            <div className="grey-text">
+                                <MDBRow>
+                                    <MDBCol>
+                                        <MDBInput
+                                        label="Title"
+                                        icon="text-height"                            
+                                        type="text"
+                                        required
+                                        value={title}
+                                        onChange={e => setTitle(e.target.value)}
+                                        />
+                                        { validationErrors.title
+                                        ? <ValidationError errorText="Please provide a valid Title."/>                                        
+                                        : null}
+                                    </MDBCol>
+                                </MDBRow>
+                                <MDBRow>
+                                    <MDBCol>
+                                        <MDBInput
+                                        type="textarea"
+                                        label="Details"
+                                        rows="2"
+                                        icon="pencil-alt"
+                                        required
+                                        value={details}
+                                        onChange={e => setDetails(e.target.value)}
+                                        />
+                                        { validationErrors.details
+                                        ? <ValidationError errorText="Please provide a valid Details."/>                                        
+                                        : null}
+                                    </MDBCol>
+                                </MDBRow>
+                                <MDBRow>
+                                    <MDBCol>
+                                        <DropDownSelect onChange={(priority) => setPriority(priority)} icon="exclamation" label="Priority" required
+                                        dropDownItems={[
+                                            {
+                                                value: "urgent",
+                                                name: "urgent"
+                                            },
+                                            {
+                                                value: "important",
+                                                name: "important"
+                                            },
+                                            {
+                                                value: "normal",
+                                                name: "normal"
+                                            }
+                                        ]}
+                                        defaultValue={priority}/>
+                                        { validationErrors.priority
+                                        ? <ValidationError errorText="Please select a valid Priority."/>                                        
+                                        : null}  
+                                    </MDBCol>
+                                </MDBRow>                           
+                                <MDBRow>
+                                    <MDBCol>
+                                        <LoadImage fileCallback={fileCallback}/>
+                                    </MDBCol>
+                                </MDBRow>                                                        
+                            </div>                
                         </form>
                     </MDBCol>
                     </MDBRow>
                 </MDBModalBody>
                 <MDBModalFooter>
-                {/* <MDBBtn color="secondary" onClick={toggle}>
-                    Close
-                </MDBBtn> */}
-                {/* <MDBBtn color="primary" onClick={addUpdate}>Save changes</MDBBtn> */}
                 <RoundedBtn color="secondary" onClick={toggle} icon="window-close" caption="Close"/>
                 <RoundedBtn color="primary" onClick={addUpdate} icon="save" caption={issueToUpdate ? "Save changes" : "Create Issue"}/>
                 </MDBModalFooter>
