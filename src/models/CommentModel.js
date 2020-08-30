fa87c6f9d32dc8725e25c2c22e91e7e76f1f42f2
yaronyes <yaronyes@hotmail.com>
2020-08-30 12:10:54 +0300

    fixed typo

--- src/models/CommentModel.js
+++ src/models/CommentModel.js
@@ -2,7 +2,7 @@ export default class CommentModel {
     constructor({ _id, text, createdBy, committee, reply, createdAt, updatedAt }) {
         this._id = _id;
         this.text = text;
-        this.createdAt = createdBy;
+        this.createdBy = createdBy;
         this.committee = committee;
         this.reply = reply;
         this.createdAt = createdAt;
